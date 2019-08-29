74f48e2347577a90f4fec624e4300efe59ca2ee1
Kalen <31481852+kalenwallin@users.noreply.github.com>
2019-08-29 12:01:34 -0500

    Update hello2.c

--- hello2.c
+++ hello2.c
@@ -9,9 +9,11 @@
 #include<stdio.h>
 
 int main(int argc, char **argv) {
-
   printf("Jarred Sinica\n");
   printf("Software engineering\n");
+
+  printf("Kalen Wallin\n");
+  printf("Computer Science\n");
 
   return 0;
 }
